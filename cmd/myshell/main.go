573788c16464ea9c1f00b4d6295829a2db70db1d
AkashRajpurohit <me@akashrajpurohit.com>
2024-08-21 19:15:17 +0530

    2nd stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,10 +7,28 @@ import (
 	"os"
 )
 
+var validCommands = []string{}
+
 func main() {
 	// Uncomment this block to pass the first stage
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading input")
+		return
+	}
+
+	// Remove newline character
+	command = command[:len(command)-1]
+
+	for _, validCommand := range validCommands {
+		if command == validCommand {
+			fmt.Println("Command found")
+			return
+		}
+	}
+
+	fmt.Printf("%s: command not found\n", command)
 }
